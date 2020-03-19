c7c23591245408f876de76b87f4a6668bf8c4bd7
sunxi1997 <2351405472@qq.com>
2020-03-19 11:49:46 +0800

    初始模板

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {},
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/user/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    个人中心
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {},
+  }
+</script>
+
+<style scoped lang="">
+
+</style>
